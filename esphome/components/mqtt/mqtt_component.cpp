e9fe23fd5dbe5f7cffea33f5d6e5a10f421a0655
Cookie987 <bili.cookie987@outlook.com>
2025-11-12 22:29:28 +0800

    revert mqtt

--- esphome/components/mqtt/mqtt_component.cpp
+++ esphome/components/mqtt/mqtt_component.cpp
@@ -28,13 +28,12 @@ std::string MQTTComponent::get_discovery_topic_(const MQTTDiscoveryInfo &discove
 
 std::string MQTTComponent::get_default_topic_for_(const std::string &suffix) const {
   const std::string &topic_prefix = global_mqtt_client->get_topic_prefix();
-  const auto mac = get_mac_address();
   if (topic_prefix.empty()) {
     // If the topic_prefix is null, the default topic should be null
     return "";
   }
 
-  return "lemonade/" + mac + "/" + this->component_type() + "/" + this->get_default_object_id_() + "/" + suffix;
+  return topic_prefix + "/" + this->component_type() + "/" + this->get_default_object_id_() + "/" + suffix;
 }
 
 std::string MQTTComponent::get_state_topic_() const {
