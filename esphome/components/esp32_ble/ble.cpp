c59285a820ff7260866455cfb9492a04a7f0b07f
Cookie987 <bili.cookie987@outlook.com>
2025-11-12 12:51:41 +0800

    ble lightsleep

--- esphome/components/esp32_ble/ble.cpp
+++ esphome/components/esp32_ble/ble.cpp
@@ -231,7 +231,11 @@ bool ESP32BLE::ble_setup_() {
     ESP_LOGE(TAG, "esp_bluedroid_enable failed: %d", err);
     return false;
   }
-
+  err = esp_bt_sleep_enable();
+  if (err != ESP_OK) {
+    ESP_LOGE(TAG, "esp_bt_sleep_enable failed: %d", err);
+    return false;
+  }
 #ifdef ESPHOME_ESP32_BLE_GAP_EVENT_HANDLER_COUNT
   err = esp_ble_gap_register_callback(ESP32BLE::gap_event_handler);
   if (err != ESP_OK) {
