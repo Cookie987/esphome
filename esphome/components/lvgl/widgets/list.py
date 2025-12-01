342f9832e1271eee3fa33d27d7e1add69cb9af10
Cookie987 <bili.cookie987@outlook.com>
2025-12-02 00:22:08 +0800

    Revert "fix(lvgl): 更新列表文本类型的基础对象类型"
    This reverts commit 3067bb8105ecf9cb42cc6c343ba28827d51eaafc.

--- esphome/components/lvgl/widgets/list.py
+++ esphome/components/lvgl/widgets/list.py
@@ -21,7 +21,7 @@ class LvListText(LvType):
     """List type that passes the clicked button's text as a parameter"""
     def __init__(self):
         super().__init__(
-            "lv_obj_t",
+            "lv_list_t",
             parents=(LvCompound,),
             largs=[(cg.std_string, "text")],
         )
