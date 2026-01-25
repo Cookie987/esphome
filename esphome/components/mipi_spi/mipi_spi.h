dc98eed163f4dfa5139f464608efa2447bd6e265
Cookie987 <bili.cookie987@outlook.com>
2026-01-25 14:09:17 +0800

    feat(mipi_spi): 新增set_madctl方法

--- esphome/components/mipi_spi/mipi_spi.h
+++ esphome/components/mipi_spi/mipi_spi.h
@@ -98,6 +98,11 @@ class MipiSpi : public display::Display,
     this->brightness_ = brightness;
     this->reset_params_();
   }
+  void set_madctl(uint8_t madctl) {
+    this->madctl_ = madctl;
+    if (this->is_ready())
+      this->write_command_(MADCTL_CMD, madctl);
+  }
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
 
   int get_width_internal() override { return WIDTH; }
