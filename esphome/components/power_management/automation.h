2d57229d55ee9f4e341e9eafffbabf69fedf618d
Cookie987 <bili.cookie987@outlook.com>
2026-02-19 20:21:47 +0800

    power management

---
+++ esphome/components/power_management/automation.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "power_management.h"
+
+namespace esphome {
+namespace power_management {
+
+template<typename... Ts> class AcquireLockAction : public Action<Ts...>, public Parented<PowerManagement> {
+ public:
+  TEMPLATABLE_VALUE(std::string, lock_type)
+  void play(Ts... x) override {
+#ifdef USE_POWER_MANAGEMENT
+    if (this->lock_type_.value(x...) == "CPU") {
+      this->parent_->acquire_lock(PowerManagementLockType::CPU);
+    } else if (this->lock_type_.value(x...) == "APB") {
+      this->parent_->acquire_lock(PowerManagementLockType::APB);
+    } else if (this->lock_type_.value(x...) == "SLP") {
+      this->parent_->acquire_lock(PowerManagementLockType::SLP);
+    }
+#endif
+  }
+};
+
+template<typename... Ts> class ReleaseLockAction : public Action<Ts...>, public Parented<PowerManagement> {
+ public:
+  TEMPLATABLE_VALUE(std::string, lock_type)
+  void play(Ts... x) override {
+#ifdef USE_POWER_MANAGEMENT
+    if (this->lock_type_.value(x...) == "CPU") {
+      this->parent_->release_lock(PowerManagementLockType::CPU);
+    } else if (this->lock_type_.value(x...) == "APB") {
+      this->parent_->release_lock(PowerManagementLockType::APB);
+    } else if (this->lock_type_.value(x...) == "SLP") {
+      this->parent_->release_lock(PowerManagementLockType::SLP);
+    }
+#endif
+  }
+};
+
+}  // namespace power_management
+}  // namespace esphome
