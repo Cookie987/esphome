5b3f0a8ea88abbf59ea6d7abf8cad21d71183310
Cookie987 <bili.cookie987@outlook.com>
2026-02-20 17:24:14 +0800

    stack over flow problem

--- esphome/components/power_management/power_management_idf.cpp
+++ esphome/components/power_management/power_management_idf.cpp
@@ -1,22 +1,23 @@
 #include "power_management.h"
-#ifdef USE_ESP_IDF
+#ifdef USE_ESP32
+#include "esphome/core/application.h"
 #include "esphome/core/log.h"
+#include <cstdio>
 
-namespace esphome {
-namespace power_management {
+namespace esphome::power_management {
 
-static const char *TAG = "power_management";
+static const char *const TAG = "power_management";
 
-// static only use is in setup
-void PowerManagement::initial_timer_callback(TimerHandle_t xTimer) {
-  void *context = pvTimerGetTimerID(xTimer);
-  PowerManagement *obj = (PowerManagement *) context;
-  obj->initial_lock_ = false;
-  obj->release_lock(PowerManagementLockUser::SELF, PowerManagementLockType::CPU);
+#if CONFIG_PM_LIGHT_SLEEP_CALLBACKS
+esp_err_t pm_after_wake_up_callback(int64_t sleep_time_us, void *arg) {
+  PowerManagement *obj = (PowerManagement *) arg;
+  obj->is_delay_aborted = true;
+  xTaskNotifyGive(obj->task_handle);
+  return ESP_OK;
 }
+#endif
 
-void PowerManagement::setup_() {
-  power_management::global_pm = this;
+void PowerManagement::setup() {
   esp_err_t rc = ESP_OK;
   // Configure PM
   int max_freq_mhz = this->max_freq_mhz_ > 0 ? this->max_freq_mhz_ : CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ;
@@ -35,171 +36,114 @@ void PowerManagement::setup_() {
 
   rc = esp_pm_configure(&pm_config);
   if (rc != 0) {
-    this->mark_failed();
     ESP_LOGE(TAG, "Failed esp_pm_configure %d", rc);
+    this->mark_failed();
     return;
   }
 
+#ifdef USE_POWER_MANAGEMENT
   // Create Locks
-  rc = esp_pm_lock_create(ESP_PM_CPU_FREQ_MAX, 0, "esphome_cpu", &this->pm_pm_lock_cpu_);
-  if (rc != ESP_OK) {
-    esp_pm_lock_delete(this->pm_pm_lock_cpu_);
-    this->pm_pm_lock_cpu_ = NULL;
-    ESP_LOGE(TAG, "Failed esp_pm_lock_create %d", rc);
-    return;
-  }
-  rc = esp_pm_lock_create(ESP_PM_APB_FREQ_MAX, 0, "esphome_apb", &this->pm_pm_lock_apb_);
-  if (rc != ESP_OK) {
-    esp_pm_lock_delete(this->pm_pm_lock_apb_);
-    this->pm_pm_lock_apb_ = NULL;
-    ESP_LOGE(TAG, "Failed esp_pm_lock_create %d", rc);
-    return;
-  }
-#if CONFIG_FREERTOS_USE_TICKLESS_IDLE
-  rc = esp_pm_lock_create(ESP_PM_NO_LIGHT_SLEEP, 0, "esphome_slp", &this->pm_pm_lock_slp_);
-  if (rc != ESP_OK) {
-    esp_pm_lock_delete(this->pm_pm_lock_slp_);
-    this->pm_pm_lock_slp_ = NULL;
-    ESP_LOGE(TAG, "Failed esp_pm_lock_create %d", rc);
-    return;
-  }
-#endif
-
-  if (this->initial_lock_duration_ > 0) {
-    this->initial_lock_ = true;
-    // Acquire Initial Lock
-    this->acquire_lock(PowerManagementLockUser::SELF, PowerManagementLockType::CPU);
-    TimerHandle_t xTimer = xTimerCreate("PM Sleep Timer", pdMS_TO_TICKS(this->initial_lock_duration_), pdFALSE, this,
-                                        PowerManagement::initial_timer_callback);
-    xTimerStart(xTimer, 0);
-  }
-}
-
-void PowerManagement::loop_() {
-  if (this->is_dump_locks_ && this->get_total_lock_count_() == 0) {
-    ESP_LOGD(TAG, "PM Locks Dumped", esp_pm_dump_locks(stdout));
-    this->is_dump_locks_ = false;
-  } else if (!this->is_dump_locks_ && this->get_total_lock_count_() > 0) {
-    this->is_dump_locks_ = true;
-  }
-}
-
-// Not Thread Safe, but read only
-int PowerManagement::get_total_lock_count_() {
-  if (this->is_ready()) {
-#if CONFIG_FREERTOS_USE_TICKLESS_IDLE
-    return this->pm_lock_counter_cpu_ + this->pm_lock_counter_apb_ + this->pm_lock_counter_slp_;
-#else
-    return this->pm_lock_counter_cpu_ + this->pm_lock_counter_apb_;
-#endif
-  }
-  return 0;
-}
-
-// Thread Safe
-void PowerManagement::acquire_lock_(PowerManagementLockUser user, PowerManagementLockType lt) {
-  if (this->is_ready()) {
-    std::lock_guard<std::mutex> lock(this->pm_lock_mutex_);
-    switch (lt) {
-      case PowerManagementLockType::CPU:
-        if (this->pm_lock_counter_cpu_ == 0) {
-          esp_err_t rc = esp_pm_lock_acquire(this->pm_pm_lock_cpu_);
-          if (rc != ESP_OK) {
-            ESP_LOGE(TAG, "Failed esp_pm_lock_acquire %d", rc);
-          }
-        }
-        pm_lock_counter_cpu_++;
-        ESP_LOGD(TAG, "Acquired Power Management CPU Lock, user: %s, cnt: %d, total_cnt: %d",
-                 power_manager_user_to_string(user), this->pm_lock_counter_cpu_, this->get_total_lock_count_());
-        break;
-      case PowerManagementLockType::APB:
-        if (this->pm_lock_counter_apb_ == 0) {
-          esp_err_t rc = esp_pm_lock_acquire(this->pm_pm_lock_apb_);
-          if (rc != ESP_OK) {
-            ESP_LOGE(TAG, "Failed esp_pm_lock_acquire %d", rc);
-          }
-        }
-        this->pm_lock_counter_apb_++;
-        ESP_LOGD(TAG, "Acquired Power Management APB Lock, user: %s, cnt: %d, total_cnt: %d",
-                 power_manager_user_to_string(user), this->pm_lock_counter_apb_, this->get_total_lock_count_());
-        break;
-      case PowerManagementLockType::SLP:
-#if CONFIG_FREERTOS_USE_TICKLESS_IDLE
-        if (this->pm_lock_counter_slp_ == 0) {
-          esp_err_t rc = esp_pm_lock_acquire(this->pm_pm_lock_slp_);
-          if (rc != ESP_OK) {
-            ESP_LOGE(TAG, "Failed esp_pm_lock_acquire %d", rc);
-          }
-        }
-        this->pm_lock_counter_slp_++;
-        ESP_LOGD(TAG, "Acquired Power Management SLP Lock, user: %s, cnt: %d, total_cnt: %d",
-                 power_manager_user_to_string(user), this->pm_lock_counter_slp_, this->get_total_lock_count_());
-#endif
-        break;
-      default:
-        ESP_LOGW(TAG, "Lock Type undefined");
-        break;
+  for (uint8_t i = 0; i < PowerManagement::PM_LOCK_ARRAY_SIZE; i++) {
+    rc = esp_pm_lock_create(this->pm_lock_types_[i], 0, power_manager_type_to_string((PowerManagementLockType) i),
+                            &(this->pm_lock_handles_[i]));
+    if (rc != ESP_OK) {
+      esp_pm_lock_delete(this->pm_lock_handles_[i]);
+      this->pm_lock_handles_[i] = NULL;
+      ESP_LOGE(TAG, "Failed esp_pm_lock_create %s %d", power_manager_type_to_string((PowerManagementLockType) i), rc);
+      return;
     }
   }
+#endif
+
+#if CONFIG_PM_LIGHT_SLEEP_CALLBACKS
+  task_handle = xTaskGetCurrentTaskHandle();
+  esp_pm_sleep_cbs_register_config_t pm_callbacks = {
+      .enter_cb = NULL,
+      .exit_cb = pm_after_wake_up_callback,
+      .enter_cb_user_arg = NULL,
+      .exit_cb_user_arg = this,
+      .enter_cb_prior = 1,
+      .exit_cb_prior = 1,
+  };
+
+  rc = esp_pm_light_sleep_register_cbs(&pm_callbacks);
+  if (rc != ESP_OK) {
+    ESP_LOGE(TAG, "Failed to register power management callbacks: %s", esp_err_to_name(rc));
+  }
+#endif
 }
 
-// Thread Safe
-void PowerManagement::release_lock_(PowerManagementLockUser user, PowerManagementLockType lt) {
-  if (this->is_ready()) {
-    std::lock_guard<std::mutex> lock(this->pm_lock_mutex_);
-    switch (lt) {
-      case PowerManagementLockType::CPU:
-        // this assumes that initial lock is cpu, prevents a button from accidentally removing lock
-        if (this->initial_lock_ && this->pm_lock_counter_cpu_ == 1) {
+void PowerManagement::loop() {
+#if CONFIG_PM_LIGHT_SLEEP_CALLBACKS
+  if (this->ready_to_sleep_()) {
+    this->is_delay_aborted = false;
+    ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(60000));
+    if (this->is_delay_aborted) {
+      esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+      switch (wakeup_reason) {
+        case ESP_SLEEP_WAKEUP_EXT0:
+          ESP_LOGV(TAG, "light sleep wakeup: RTC_IO (EXT0 - single pad)");
           break;
-        }
-        if (this->pm_lock_counter_cpu_ == 1) {
-          esp_err_t rc = esp_pm_lock_release(this->pm_pm_lock_cpu_);
-          if (rc != ESP_OK) {
-            ESP_LOGE(TAG, "Failed esp_pm_lock_release %d", rc);
-          }
-        }
-        if (this->pm_lock_counter_cpu_ > 0) {
-          this->pm_lock_counter_cpu_--;
-        }
-        ESP_LOGD(TAG, "Released Power Management CPU Lock, user: %s, cnt: %d, total_cnt: %d",
-                 power_manager_user_to_string(user), this->pm_lock_counter_cpu_, this->get_total_lock_count_());
-        break;
-      case PowerManagementLockType::APB:
-        if (this->pm_lock_counter_apb_ == 1) {
-          esp_err_t rc = esp_pm_lock_release(this->pm_pm_lock_apb_);
-          if (rc != ESP_OK) {
-            ESP_LOGE(TAG, "Failed esp_pm_lock_release %d", rc);
-          }
-        }
-        if (this->pm_lock_counter_apb_ > 0) {
-          this->pm_lock_counter_apb_--;
-        }
-        ESP_LOGD(TAG, "Released Power Management APB Lock, user: %s, cnt: %d, total_cnt: %d",
-                 power_manager_user_to_string(user), this->pm_lock_counter_apb_, this->get_total_lock_count_());
-        break;
-      case PowerManagementLockType::SLP:
-#if CONFIG_FREERTOS_USE_TICKLESS_IDLE
-        if (this->pm_lock_counter_slp_ == 1) {
-          esp_err_t rc = esp_pm_lock_release(this->pm_pm_lock_slp_);
-          if (rc != ESP_OK) {
-            ESP_LOGE(TAG, "Failed esp_pm_lock_release %d", rc);
-          }
-        }
-        if (this->pm_lock_counter_slp_ > 0) {
-          this->pm_lock_counter_slp_--;
-        }
-        ESP_LOGD(TAG, "Released Power Management SLP Lock, user: %s, cnt: %d, total_cnt: %d",
-                 power_manager_user_to_string(user), this->pm_lock_counter_slp_, this->get_total_lock_count_());
-#endif
-        break;
-      default:
-        ESP_LOGW(TAG, "Lock Type undefined");
+        case ESP_SLEEP_WAKEUP_EXT1:
+          ESP_LOGV(TAG, "light sleep wakeup: RTC_IO (EXT1 - multiple pads)");
+          break;
+        case ESP_SLEEP_WAKEUP_TIMER:
+          ESP_LOGV(TAG, "light sleep wakeup: Timer");
+          break;
+        case ESP_SLEEP_WAKEUP_TOUCHPAD:
+          ESP_LOGV(TAG, "light sleep wakeup: Touchpad");
+          break;
+        case ESP_SLEEP_WAKEUP_ULP:
+          ESP_LOGV(TAG, "light sleep wakeup: ULP program");
+          break;
+        case ESP_SLEEP_WAKEUP_GPIO:
+          ESP_LOGV(TAG, "light sleep wakeup: GPIO (Light Sleep only)");
+          break;
+        case ESP_SLEEP_WAKEUP_UART:
+          ESP_LOGV(TAG, "light sleep wakeup: UART");
+          break;
+        default:
+          ESP_LOGV(TAG, "light sleep wakeup unknown: %d", wakeup_reason);
+          break;
+      }
     }
+  }
+#endif
+}
+
+#ifdef USE_POWER_MANAGEMENT
+// Thread Safe
+void PowerManagement::acquire_lock(PowerManagementLockType lt) {
+  if (this->is_ready()) {
+    std::lock_guard<std::mutex> lock(this->pm_lock_mutex_);
+    esp_err_t rc = esp_pm_lock_acquire(this->pm_lock_handles_[lt]);
+    if (rc != ESP_OK) {
+      ESP_LOGE(TAG, "Failed esp_pm_lock_acquire %s %d", power_manager_type_to_string(lt), rc);
+      return;
+    }
+    ESP_LOGD(TAG, "Acquired pm lock: %s", power_manager_type_to_string(lt));
   }
 }
 
-void PowerManagement::dump_config_() {
+// Thread Safe
+void PowerManagement::release_lock(PowerManagementLockType lt) {
+  if (this->is_ready()) {
+    std::lock_guard<std::mutex> lock(this->pm_lock_mutex_);
+    esp_err_t rc = esp_pm_lock_release(this->pm_lock_handles_[lt]);
+    if (rc != ESP_OK) {
+      ESP_LOGE(TAG, "Failed esp_pm_lock_release %s %d", power_manager_type_to_string(lt), rc);
+      return;
+    }
+    ESP_LOGD(TAG, "Released pm lock: %s", power_manager_type_to_string(lt));
+  }
+}
+#endif
+
+void PowerManagement::dump_config() {
+  ESP_LOGCONFIG(TAG, "Power Management:");
+#ifdef USE_POWER_MANAGEMENT
+  ESP_LOGCONFIG(TAG, "  EspHome Locks available");
+#endif
 #if CONFIG_FREERTOS_USE_TICKLESS_IDLE
   ESP_LOGCONFIG(TAG, "  Light Sleep Enabled");
 #if CONFIG_ESP_SLEEP_POWER_DOWN_FLASH
@@ -211,15 +155,55 @@ void PowerManagement::dump_config_() {
 #if CONFIG_IEEE802154_SLEEP_ENABLE
   ESP_LOGCONFIG(TAG, "  ieee802154 Sleep Enabled");
 #endif
-#if CONF_PM_PROFILING
+#if CONFIG_PM_PROFILING
   ESP_LOGCONFIG(TAG, "  PM Profiling Enabled");
 #endif
-#if CONF_PM_TRACE
+#if CONFIG_PM_TRACE
   ESP_LOGCONFIG(TAG, "  PM Trace Enabled");
 #endif
 #endif
 }
 
-}  // namespace power_management
-}  // namespace esphome
+static const size_t PM_BUF_SIZE = 1024;
+// Todo: use pm function esp_pm_get_lock_stats_all when it is available.
+bool PowerManagement::ready_to_sleep_() {
+  char pm_buffer[PM_BUF_SIZE];
+  int32_t acquired = 0;
+
+  FILE *f = fmemopen(pm_buffer, PM_BUF_SIZE, "w");
+  if (f == NULL) {
+    ESP_LOGE(TAG, "count_pm_locks, fmemopen failed %d", errno);
+    return false;
+  }
+
+  esp_pm_dump_locks(f);
+  fclose(f);
+
+  if (pm_buffer[0] == '\0') {
+    ESP_LOGE(TAG, "esp_pm_dump_locks produced no output");
+    return false;
+  }
+
+  char *line_saveptr;
+  char *word_saveptr;
+  char *line = strtok_r(pm_buffer, "\n", &line_saveptr);  // NOLINT(clang-analyzer-deadcode.DeadStores)
+  while ((line = strtok_r(NULL, "\n", &line_saveptr)) != NULL) {
+    if (strncmp(line, "Mode", 4) == 0) {
+      break;
+    }
+    if (strncmp(line, "Name", 4) != 0 && strncmp(line, "Lock", 4) != 0) {
+      char *word = strtok_r(line, " ", &word_saveptr);
+      for (int i = 0; i < 3; i++) {
+        word = strtok_r(NULL, " ", &word_saveptr);
+      }
+      // at 4th word
+      if (word != NULL) {
+        acquired += strtol(word, NULL, 10);
+      }
+    }
+  }
+  return (acquired < 2);
+}
+
+}  // namespace esphome::power_management
 #endif
