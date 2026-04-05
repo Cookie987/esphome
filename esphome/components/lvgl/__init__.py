c8699c93f8d1a32be26ea27b0daf76336ed1a71b
Cookie987 <cookie987@cookie987.top>
2026-04-05 13:37:29 +0800

    支持tiny ttf

--- esphome/components/lvgl/__init__.py
+++ esphome/components/lvgl/__init__.py
@@ -127,9 +127,8 @@ def generate_lv_conf_h():
     definitions.append("#define LV_IMG_CACHE_DEF_SIZE 10")
     definitions.append("#define LV_FS_POSIX_LETTER 'A'")
     definitions.append("#define LV_LABEL_LONG_TXT_HINT 1")
-    # definitions.append("#define LV_USE_IME_PINYIN 1")
-    # definitions.append("#define LV_IME_PINYIN_USE_DEFAULT_DICT 1")
-    # definitions.append("#define LV_IME_PINYIN_USE_K9_MODE 1")
+    definitions.append("#define LV_USE_TINY_TTF 1")
+    definitions.append("LV_TINY_TTF_FILE_SUPPORT 1")
     definitions.sort()
     return LV_CONF_H_FORMAT.format("\n".join(definitions))
 
