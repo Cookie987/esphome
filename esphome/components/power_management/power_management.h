53dc0e5f70c9e59ab56b4345cf57858a14c040ff
Cookie987 <cookie987@cookie987.top>
2026-04-27 23:42:18 +0800

    电源管理优化

--- esphome/components/power_management/power_management.h
+++ esphome/components/power_management/power_management.h
@@ -26,7 +26,6 @@ class PowerManagement : public Component {
  public:
   float get_setup_priority() const override { return setup_priority::BUS; }
   void setup() override;
-  void loop() override;
   void dump_config() override;
   void set_max_freq_mhz(uint32_t max_freq_mhz) { this->max_freq_mhz_ = max_freq_mhz; }
   void set_min_freq_mhz(uint32_t min_freq_mhz) { this->min_freq_mhz_ = min_freq_mhz; }
